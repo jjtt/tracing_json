7435210a106d99ce350961607b41b6b2d27a9260
jjtt <3908945+jjtt@users.noreply.github.com>
2023-06-21 21:44:39 +0300

    Add tests and cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -171,21 +171,54 @@ impl<'a> tracing::field::Visit for JsonVisitor<'a> {
     }
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::sync::{Arc, Mutex};
+    use tracing::field;
     use tracing::subscriber::with_default;
     use tracing_subscriber::Registry;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// A helper method for asserting a serde::Value matches expectations
+    fn assert_json_timestamp_name(
+        expected: Value,
+        name_value_prefix: &str,
+        before: &OffsetDateTime,
+        value: &mut Value,
+    ) {
+        let map = value.as_object_mut().unwrap();
+        assert!(map.contains_key("name"));
+        assert!(map
+            .remove("name")
+            .expect("should contain field 'name'")
+            .as_str()
+            .expect("field 'name' should be a string")
+            .starts_with(name_value_prefix));
+
+        assert!(map.contains_key("timestamp"));
+        let timestamp = map
+            .remove("timestamp")
+            .expect("should contain field 'timestamp'")
+            .as_str()
+            .expect("field 'timestamp' should be a string")
+            .to_string();
+        let parsed = OffsetDateTime::parse(&timestamp, &Iso8601::DEFAULT)
+            .expect("timestamp should be parseable");
+        assert!(
+            parsed.ge(before),
+            "timestamp ({}) must not be before {}",
+            timestamp,
+            before
+        );
+        let now = &OffsetDateTime::now_utc();
+        assert!(
+            parsed.le(now),
+            "timestamp ({}) must not be after {}",
+            timestamp,
+            now
+        );
+
+        assert_eq!(expected, *value)
     }
 
     struct TestOutput {
@@ -212,6 +245,8 @@ mod tests {
 
         tracing::info!("BEFORE");
 
+        let before = OffsetDateTime::now_utc();
+
         with_default(subscriber, || {
             let _span1 = tracing::info_span!("Top level", field_top = 0).entered();
             tracing::info!(field_event = "from event", "FOOBAR");
@@ -220,23 +255,10 @@ mod tests {
 
         tracing::info!("AFTER");
 
-        let data = data.lock().unwrap();
-        for d in (*data).iter() {
-            dbg!(d);
-        }
-        let mut iter = (*data).iter();
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
 
-        let mut first = iter.next().unwrap().clone();
-        let first_map = first.as_object_mut().unwrap();
-        assert!(first_map.contains_key("name"));
-        assert!(first_map
-            .remove("name")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .starts_with("event src/lib.rs:"));
-
-        assert_eq!(
+        assert_json_timestamp_name(
             serde_json::json!({
                 "target": "tracing_json::tests",
                 "level": "Level(Info)",
@@ -244,17 +266,280 @@ mod tests {
                 "field_top": 0,
                 "field_event": "from event"
             }),
-            first
-        )
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Error)",
+                "message": "BAZ",
+                "field_top": 0,
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
+    }
+
+    #[test]
+    fn two_spans_different_fields() {
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
+
+        with_default(subscriber, || {
+            let _span1 = tracing::info_span!("Top level", field_top = 0).entered();
+            let _span2 = tracing::info_span!("Second level", field_second = 1).entered();
+            tracing::info!(field_event = "from event", "FOOBAR");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "FOOBAR",
+                "field_top": 0,
+                "field_second": 1,
+                "field_event": "from event"
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
+    }
+
+    #[test]
+    fn two_spans_same_fields() {
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
+
+        with_default(subscriber, || {
+            let _span1 = tracing::info_span!("Top level", field_overwrite = 0).entered();
+            let _span2 = tracing::info_span!("Second level", field_overwrite = 1).entered();
+            tracing::info!(field_event = "from event", "FOOBAR");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "FOOBAR",
+                "field_overwrite": 1,
+                "field_event": "from event"
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
+    }
+
+    #[test]
+    fn two_spans_same_fields_including_event() {
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
+
+        with_default(subscriber, || {
+            let _span1 = tracing::info_span!("Top level", field_overwrite = 0).entered();
+            let _span2 = tracing::info_span!("Second level", field_overwrite = 1).entered();
+            tracing::info!(field_overwrite = "from event", "FOOBAR");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "FOOBAR",
+                "field_overwrite": "from event"
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
+    }
+
+    #[test]
+    fn two_events_from_two_spans() {
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
+
+        with_default(subscriber, || {
+            let _span1 = tracing::info_span!("Top level", field_top = 0).entered();
+            tracing::info!(field_event = "from event one", "ONE");
+            let _span2 = tracing::info_span!("Second level", field_second = 1).entered();
+            tracing::info!(field_event = "from event two", "TWO");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "ONE",
+                "field_top": 0,
+                "field_event": "from event one"
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "TWO",
+                "field_top": 0,
+                "field_second": 1,
+                "field_event": "from event two"
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
+    }
+
+    #[test]
+    fn one_span_recorded_field() {
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
+
+        with_default(subscriber, || {
+            let span = tracing::info_span!("A span", span_field = 0, recorded_field = field::Empty);
+            span.record("recorded_field", "foo");
+            let _span = span.entered();
+            tracing::info!(event_field = 1.1, "FOOBAR");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "FOOBAR",
+                "event_field": 1.1,
+                "recorded_field": "foo",
+                "span_field": 0,
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
+    }
+
+    #[test]
+    fn one_span_recorded_field_overwriting_initial_field() {
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
+
+        with_default(subscriber, || {
+            let span = tracing::info_span!("A span", span_field = 0);
+            span.record("span_field", "foo");
+            let _span = span.entered();
+            tracing::info!(event_field = 1.1, "FOOBAR");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "FOOBAR",
+                "event_field": 1.1,
+                "span_field": "foo",
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
     }
 
     #[test]
     fn without_any_spans() {
-        let subscriber = Registry::default().with(JsonLayer::default());
+        let data = Arc::new(Mutex::new(vec![]));
+        let layer = JsonLayer {
+            output: TestOutput { data: data.clone() },
+        };
+
+        let subscriber = Registry::default().with(layer);
+
+        let before = OffsetDateTime::now_utc();
 
         with_default(subscriber, || {
-            // TODO: Should not fail in on_event()
-            tracing::info!("FOOBAR");
-        })
+            tracing::info!(event_field = 1.1, "FOOBAR");
+        });
+
+        let mut data = data.lock().unwrap();
+        let mut iter = (*data).iter_mut();
+
+        assert_json_timestamp_name(
+            serde_json::json!({
+                "target": "tracing_json::tests",
+                "level": "Level(Info)",
+                "message": "FOOBAR",
+                "event_field": 1.1,
+            }),
+            "event src/lib.rs:",
+            &before,
+            iter.next().unwrap(),
+        );
+        assert_eq!(None, iter.next(), "No more logged events");
     }
 }
